efa76dfd8d5dc5ec2f470f8447142082773d579d
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-08-20 12:58:48 +0100

    Added processing of reference data structure
    Added some more processing of the returned data structure

--- run.pl
+++ run.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use JSON;
 use Data::Dump qw(dump);
 use Cwd qw( abs_path );
 use File::Basename qw( dirname );
@@ -8,4 +9,13 @@ use lib dirname(abs_path($0));
 use EnsemblRestQueries;
 
 my $ensembl_id = 'ENSG00000157764';
-dump EnsemblRestQueries::get_xrefs_for_ensembl_id($ensembl_id);
+my $gene_xrefs = EnsemblRestQueries::get_xrefs_for_ensembl_id($ensembl_id);
+my $gene_xrefs_ds = decode_json($gene_xrefs); 
+my $ref_type = ref $gene_xrefs_ds;
+print "$ref_type\n";
+my $first_element = $gene_xrefs_ds->[0];
+print "The gene xrefs are:\n";
+foreach my $gene_xref (@{$gene_xrefs_ds}) {
+    dump($gene_xref);
+}
+
