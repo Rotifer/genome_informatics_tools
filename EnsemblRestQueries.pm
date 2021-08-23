8ed376cebdde7b327c8d354618fb319e1477e9f3
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-08-23 10:13:57 +0100

    Added documentation
    Added documentation to modules

--- EnsemblRestQueries.pm
+++ EnsemblRestQueries.pm
@@ -1,9 +1,12 @@
 package EnsemblRestQueries;
 use HTTP::Tiny;
 
+# A module to execute Ensemble REST API calls for specific end-points
 
-my $base_url = "https://rest.ensembl.org/";
+my $base_url = "https://rest.ensembl.org/"; # Need a better way of setting this value
 
+# Return a JSON string for a given end-point.
+# All end-oint calls are routed through this subroutine
 sub get_json_for_endpoint {
     my $endpoint_url = shift;
     my $url = $base_url . $endpoint_url;
@@ -11,12 +14,13 @@ sub get_json_for_endpoint {
     my $response = $http->get($url, {
                    headers => { 'Content-type' => 'application/json' }
          });
-    die "URL: $url call failed!\n" unless $response->{success};
+    die "URL: $url call failed! Error: $!\n" unless $response->{success};
     if(length $response->{content}) {
         return $response->{content};
     }
 }
 
+# Return all cross-references for a given Ensembl ID, for example: gene, transcript, variant for any species.
 sub get_xrefs_for_ensembl_id {
     my $ensembl_id = shift;
     my $end_point_url = "xrefs/id/$ensembl_id?";
--- ExtUtils.pm
+++ ExtUtils.pm
@@ -1,6 +1,10 @@
 package ExtUtils;
 
 
+# A module to provide information on commandline tools
+
+
+# Determine if the given tool name is available. Returns boolean
 sub is_util_available {
     my $util_name = shift;
     my $which_cmd = "which $util_name";
@@ -12,6 +16,7 @@ sub is_util_available {
     return 0;
 }
 
+# Return the version of the given tool name. Assumes it supports --version option
 sub util_version {
     my $util_name = shift;
     my $version_cmd = "$util_name --version";
